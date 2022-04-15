db51a92793639118ab4d7753b0d459ad6671cbb7
Justin <Justin@DESKTOP-LUQUUDK>
2022-04-15 00:13:51 -0400

    Creacion de metodo para calcular volumenes

--- ProyectoFinalG06/src/logico/Prisma.java
+++ ProyectoFinalG06/src/logico/Prisma.java
@@ -1,6 +1,7 @@
 package logico;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public abstract class Prisma implements Serializable {
 
@@ -11,6 +12,7 @@ public abstract class Prisma implements Serializable {
 	private static final long serialVersionUID = 1L;
 	protected float base;
 	protected int color;
+	private ArrayList<Prisma>misPrismas;
 	
 	public Prisma(float base, int color) {
 		super();
@@ -33,6 +35,43 @@ public abstract class Prisma implements Serializable {
 	public void setColor(int color) {
 		this.color = color;
 	}
-
+	
+	public float calcularVolumenes(){
+		
+	Prisma aux=null;
+	
+	float total = 0;
+	
+		for (Prisma prisma : misPrismas) {
+			
+			if(prisma instanceof Cuadrado) {
+				total+=(((Cuadrado)aux).getBase() * ((Cuadrado)aux).getAltura() * ((Cuadrado)aux).getProfundidad());	
+			}
+			
+			else if (prisma instanceof Rectangulo) {
+				total+=(((Rectangulo)aux).getBase() * ((Rectangulo)aux).getAltura() * ((Rectangulo)aux).getProfundidad());
+			}
+			
+			else if (prisma instanceof Triangulo) {
+				total+=((1/3)*(((Triangulo)aux).getAltura()) * (((Triangulo)aux).getBase()) * (Math.PI));
+			}
+			
+			else if (prisma instanceof Cilindro) {
+				total+=((Math.PI) * (((Cilindro)aux).getBase()) * (((Cilindro)aux).getAltura()));
+				
+			}
+			
+			else if (prisma instanceof Esfera) {
+				total+=((4/3) * (Math.PI) * (((Esfera)aux).getBase() + ((Esfera)aux).getBase()));
+				
+			}
+			
+		}
+		
+		
+		return base;
+		
+		
+	}
 
 }
