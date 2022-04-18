daa43a506f53f48e3da3661b9d06361a26f83db8
ronal <ronal@LAPTOP-5P34Q0RQ>
2022-04-18 15:08:21 -0400

    Mejorando la parte visual

--- ProyectoFinalG06/src/visual/Inicio.java
+++ ProyectoFinalG06/src/visual/Inicio.java
@@ -34,6 +34,9 @@ import javax.swing.ImageIcon;
 import java.awt.Dialog.ModalityType;
 import java.awt.Dialog.ModalExclusionType;
 import java.awt.Toolkit;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JSeparator;
 
 public class Inicio extends JDialog {
 
@@ -41,6 +44,10 @@ public class Inicio extends JDialog {
 	private JTextField textUsuario;
 	private JButton btnNewButton_1;
 	private JPasswordField textContra;
+	private JPanel panelExit;
+	private JLabel lblFoto;
+	private JLabel lblExit;
+	private JLabel lblMostrar;
 
 	/**
 	 * Launch the application.
@@ -95,8 +102,10 @@ public class Inicio extends JDialog {
 	 * Create the dialog.
 	 */
 	public Inicio() {
+		setUndecorated(true);
+		setResizable(false);
 		setIconImage(Toolkit.getDefaultToolkit().getImage(Inicio.class.getResource("/imagenes/usuario (3).png")));
-		setBounds(100, 100, 551, 491);
+		setBounds(100, 100, 869, 571);
 		getContentPane().setLayout(new BorderLayout());
 		setLocationRelativeTo(null);
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -104,55 +113,167 @@ public class Inicio extends JDialog {
 		contentPanel.setLayout(new BorderLayout(0, 0));
 		{
 			JPanel panel = new JPanel();
-			panel.setBackground(Color.BLACK);
+			panel.setBackground(Color.WHITE);
 			panel.setBorder(null);
 			contentPanel.add(panel, BorderLayout.CENTER);
 			panel.setLayout(null);
 			
 			JPanel panel_1 = new JPanel();
-			panel_1.setBackground(Color.CYAN);
-			panel_1.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
-			panel_1.setBounds(23, 25, 476, 374);
+			panel_1.setBackground(Color.WHITE);
+			panel_1.setBorder(null);
+			panel_1.setBounds(12, 78, 514, 436);
 			panel.add(panel_1);
 			panel_1.setLayout(null);
 			
 			JLabel lblContrasen = new JLabel("");
 			lblContrasen.setIcon(new ImageIcon(Inicio.class.getResource("/imagenes/bloquear (1).png")));
-			lblContrasen.setBounds(34, 143, 94, 58);
+			lblContrasen.setBounds(130, 162, 94, 64);
 			panel_1.add(lblContrasen);
 			lblContrasen.setFont(new Font("Rockwell", Font.PLAIN, 18));
 			
 			JLabel lblNewLabel = new JLabel("");
 			lblNewLabel.setIcon(new ImageIcon(Inicio.class.getResource("/imagenes/usuario (3).png")));
-			lblNewLabel.setBounds(34, 49, 94, 64);
+			lblNewLabel.setBounds(130, 22, 94, 64);
 			panel_1.add(lblNewLabel);
 			lblNewLabel.setFont(new Font("Rockwell", Font.PLAIN, 18));
 			
 			textUsuario = new JTextField();
-			textUsuario.setBounds(143, 68, 283, 33);
+			textUsuario.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mousePressed(MouseEvent arg0) {
+					if(textUsuario.getText().equals("Ingrese su nombre de usuario")) {
+						textUsuario.setText("");
+						textUsuario.setForeground(Color.BLACK);
+					}
+					if(textContra.getText().isEmpty()) {
+						textContra.setText("********");
+						textContra.setForeground(Color.GRAY);
+					}
+				}
+			});
+			
+			textUsuario.setBorder(null);
+			textUsuario.setForeground(Color.LIGHT_GRAY);
+			textUsuario.setText("Ingrese su nombre de usuario");
+			textUsuario.setBounds(130, 99, 269, 33);
 			panel_1.add(textUsuario);
 			textUsuario.setColumns(10);
 			
 			btnNewButton_1 = new JButton("");
+			btnNewButton_1.setBackground(Color.WHITE);
 			btnNewButton_1.setIcon(new ImageIcon(Inicio.class.getResource("/imagenes/iniciar-sesion (1).png")));
-			btnNewButton_1.setBounds(105, 255, 94, 73);
+			btnNewButton_1.setBounds(214, 322, 94, 73);
 			panel_1.add(btnNewButton_1);
 			
 			textContra = new JPasswordField();
-			textContra.setBounds(143, 156, 283, 33);
-			panel_1.add(textContra);
-			{
-				JButton cancelButton = new JButton("");
-				cancelButton.setIcon(new ImageIcon(Inicio.class.getResource("/imagenes/cerrar-sesion (1).png")));
-				cancelButton.setBounds(287, 255, 94, 73);
-				panel_1.add(cancelButton);
-				cancelButton.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent arg0) {
-						dispose();
+			textContra.setEchoChar('*');
+			textContra.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mousePressed(MouseEvent e) {
+					if(textContra.getText().equals("********")) {
+					textContra.setText("");
+					textContra.setForeground(Color.BLACK);
 					}
-				});
-				cancelButton.setActionCommand("Cancel");
-			}
+				if(textUsuario.getText().isEmpty()) {
+					textUsuario.setText("Ingrese su nombre de usuario");
+					textUsuario.setForeground(Color.GRAY);
+				}
+				
+				}
+			});
+			textContra.setForeground(Color.LIGHT_GRAY);
+			textContra.setText("********");
+			textContra.setBorder(null);
+			textContra.setToolTipText("");
+			textContra.setBounds(130, 239, 269, 33);
+			panel_1.add(textContra);
+			
+			JSeparator separator = new JSeparator();
+			separator.setBounds(130, 137, 251, 12);
+			panel_1.add(separator);
+			
+			JSeparator separator_1 = new JSeparator();
+			separator_1.setBounds(130, 274, 251, 12);
+			panel_1.add(separator_1);
+			
+			lblMostrar = new JLabel("");
+			lblMostrar.setForeground(Color.WHITE);
+			lblMostrar.setBackground(Color.WHITE);
+			lblMostrar.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseEntered(MouseEvent e) {
+					if(!textContra.getText().equals("********")) {
+						textContra.setEchoChar((char)0);
+						lblMostrar.setBackground(Color.LIGHT_GRAY);
+						
+					}
+					
+				}
+				@Override
+				public void mouseExited(MouseEvent e) {
+					if(!textContra.getText().equals("********")) {
+						textContra.setEchoChar('*');
+						lblMostrar.setBackground(Color.WHITE);
+					}
+				}
+			});
+			lblMostrar.setIcon(new ImageIcon(Inicio.class.getResource("/imagenes/boton-de-visibilidad.png")));
+			lblMostrar.setBounds(407, 256, 37, 30);
+			panel_1.add(lblMostrar);
+			
+			JPanel panel_2 = new JPanel();
+			panel_2.setBackground(Color.WHITE);
+			panel_2.setBounds(0, 0, 530, 27);
+			panel.add(panel_2);
+			panel_2.setLayout(null);
+			
+			panelExit = new JPanel();
+			panelExit.setBackground(Color.WHITE);
+			panelExit.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseEntered(MouseEvent arg0) {
+					panelExit.setBackground(Color.RED);
+					lblExit.setForeground(Color.WHITE);
+					
+					
+				}
+				@Override
+				public void mouseExited(MouseEvent e) {
+					panelExit.setBackground(Color.WHITE);
+					lblExit.setForeground(Color.BLACK);
+				}
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					System.exit(0);
+				}
+			});
+			panelExit.setBounds(0, 0, 34, 27);
+			panel_2.add(panelExit);
+			
+			lblExit = new JLabel("X");
+			lblExit.setFont(new Font("Trebuchet MS", Font.PLAIN, 17));
+			panelExit.add(lblExit);
+			
+			JLabel lblNewLabel_1 = new JLabel("");
+			lblNewLabel_1.setIcon(new ImageIcon(Inicio.class.getResource("/imagenes/prisma (2).png")));
+			lblNewLabel_1.setBounds(642, 78, 159, 156);
+			panel.add(lblNewLabel_1);
+			
+			JLabel lblNewLabel_2 = new JLabel("PRISMA 3D");
+			lblNewLabel_2.setForeground(Color.WHITE);
+			lblNewLabel_2.setFont(new Font("Tw Cen MT Condensed", Font.BOLD, 28));
+			lblNewLabel_2.setBounds(642, 229, 129, 45);
+			panel.add(lblNewLabel_2);
+			
+			lblFoto = new JLabel("");
+			lblFoto.setBounds(538, 0, 321, 551);
+			panel.add(lblFoto);
+			lblFoto.setIcon(new ImageIcon(Inicio.class.getResource("/imagenes/blue-light-background-12465.jpg")));
+			
+			JLabel lblNewLabel_3 = new JLabel("INICIAR SECCION");
+			lblNewLabel_3.setFont(new Font("Myanmar Text", Font.BOLD, 25));
+			lblNewLabel_3.setBounds(149, 40, 276, 35);
+			panel.add(lblNewLabel_3);
 			btnNewButton_1.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
 					String aux = textUsuario.getText();
--- ProyectoFinalG06/src/visual/RegistroUsuario.java
+++ ProyectoFinalG06/src/visual/RegistroUsuario.java
@@ -19,16 +19,29 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.ImageIcon;
 import java.awt.Toolkit;
+import java.awt.Color;
+import javax.swing.JPasswordField;
+import javax.swing.JSeparator;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.LineBorder;
+import java.awt.Font;
 
 public class RegistroUsuario extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
 	private JTextField textUsuario;
-	private JTextField textContra;
-	private JTextField textContraConfirm;
 	private JRadioButton rdbProfesor;
 	private JRadioButton rdbEstudiante;
 	private String tipo;
+	private JPasswordField textContra;
+	private JPasswordField textContraConfirm;
+	private JPanel panelExit;
+	private JLabel xExit;
+	private JLabel lblEstudiante;
+	private JLabel lblProfesor;
+	private JLabel lblNewLabel_1;
 
 	/**
 	 * Launch the application.
@@ -47,84 +60,177 @@ public class RegistroUsuario extends JDialog {
 	 * Create the dialog.
 	 */
 	public RegistroUsuario() {
+		setUndecorated(true);
+		setResizable(false);
 		setIconImage(Toolkit.getDefaultToolkit().getImage(RegistroUsuario.class.getResource("/imagenes/registros-medicos.png")));
-		setBounds(100, 100, 513, 401);
+		setBounds(100, 100, 513, 377);
 		setLocationRelativeTo(null);
 		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBackground(Color.BLUE);
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
 		
 		JPanel panel = new JPanel();
-		panel.setBorder(new TitledBorder(null, "Registrate", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panel.setBounds(72, 27, 351, 265);
+		panel.setBackground(Color.WHITE);
+		panel.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panel.setBounds(21, 42, 471, 292);
 		contentPanel.add(panel);
 		panel.setLayout(null);
 		
 		textUsuario = new JTextField();
-		textUsuario.setBounds(137, 27, 187, 27);
+		textUsuario.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				if(textUsuario.getText().equals("Ingrese el nombre de usuario")) {
+					textUsuario.setText("");
+					textUsuario.setForeground(Color.BLACK);
+				}
+				if(textContra.getText().isEmpty()) {
+					textContra.setText("********");
+					textContra.setForeground(Color.GRAY);
+				}
+				if(textContraConfirm.getText().isEmpty()) {
+					textContraConfirm.setText("********");
+					textContraConfirm.setForeground(Color.GRAY);
+				}
+			}
+		});
+		textUsuario.setForeground(Color.GRAY);
+		textUsuario.setText("Ingrese el nombre de usuario");
+		textUsuario.setBorder(null);
+		textUsuario.setBounds(137, 52, 187, 27);
 		panel.add(textUsuario);
 		textUsuario.setColumns(10);
 		
 		JLabel lblNewLabel = new JLabel("Usuario:");
-		lblNewLabel.setBounds(33, 32, 56, 16);
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblNewLabel.setBounds(137, 23, 56, 16);
 		panel.add(lblNewLabel);
 		
 		JLabel lblPassword = new JLabel("Password:");
-		lblPassword.setBounds(33, 87, 69, 16);
+		lblPassword.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblPassword.setBounds(137, 92, 69, 16);
 		panel.add(lblPassword);
 		
-		textContra = new JTextField();
-		textContra.setColumns(10);
-		textContra.setBounds(137, 82, 187, 27);
-		panel.add(textContra);
-		
 		JLabel lblPassword_1 = new JLabel("Password:");
-		lblPassword_1.setBounds(33, 144, 69, 16);
+		lblPassword_1.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblPassword_1.setBounds(137, 161, 69, 16);
 		panel.add(lblPassword_1);
 		
-		textContraConfirm = new JTextField();
-		textContraConfirm.setColumns(10);
-		textContraConfirm.setBounds(137, 139, 187, 27);
-		panel.add(textContraConfirm);
-		
 		rdbEstudiante = new JRadioButton("Estudiante");
+		rdbEstudiante.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				rdbEstudiante.setForeground(Color.BLACK);
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				rdbEstudiante.setForeground(Color.GRAY);
+			}
+		});
+		rdbEstudiante.setForeground(Color.LIGHT_GRAY);
+		rdbEstudiante.setBackground(Color.WHITE);
 		rdbEstudiante.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				rdbProfesor.setSelected(false);
 				tipo = "Estudiante";
+				lblEstudiante.setVisible(true);
+				lblProfesor.setVisible(false);
 				
 			}
 		});
-		rdbEstudiante.setBounds(54, 193, 127, 25);
+		rdbEstudiante.setBounds(22, 241, 127, 25);
 		panel.add(rdbEstudiante);
 		
 		rdbProfesor = new JRadioButton("Profesor");
+		rdbProfesor.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				rdbProfesor.setForeground(Color.BLACK);
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				rdbProfesor.setForeground(Color.GRAY);
+			}
+		});
+		rdbProfesor.setForeground(Color.LIGHT_GRAY);
+		rdbProfesor.setBackground(Color.WHITE);
 		rdbProfesor.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				rdbEstudiante.setSelected(false);
 				tipo = "Profesor";
+				lblEstudiante.setVisible(false);
+				lblProfesor.setVisible(true);
 			}
 		});
-		rdbProfesor.setBounds(197, 193, 127, 25);
+		rdbProfesor.setBounds(336, 241, 127, 25);
 		panel.add(rdbProfesor);
-		{
-			JButton cancelButton = new JButton("");
-			cancelButton.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent arg0) {
-					dispose();
+		
+		textContra = new JPasswordField();
+		textContra.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				if(textUsuario.getText().isEmpty()) {
+					textUsuario.setText("Ingrese el nombre de usuario");
+					textUsuario.setForeground(Color.GRAY);
 				}
-			});
-			cancelButton.setBounds(444, 318, 51, 36);
-			contentPanel.add(cancelButton);
-			cancelButton.setIcon(new ImageIcon(RegistroUsuario.class.getResource("/imagenes/flecha-hacia-atras (2).png")));
-			cancelButton.setActionCommand("Cancel");
-		}
+				if(textContra.getText().equals("********")) {
+					textContra.setText("");
+					textContra.setForeground(Color.BLACK);
+				}
+				if(textContraConfirm.getText().isEmpty()) {
+					textContraConfirm.setText("********");
+					textContraConfirm.setForeground(Color.GRAY);
+				}
+			}
+		});
+		textContra.setForeground(Color.GRAY);
+		textContra.setText("********");
+		textContra.setBorder(null);
+		textContra.setBounds(137, 121, 187, 27);
+		panel.add(textContra);
+		
+		textContraConfirm = new JPasswordField();
+		textContraConfirm.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				if(textUsuario.getText().isEmpty()) {
+					textUsuario.setText("Ingrese el nombre de usuario");
+					textUsuario.setForeground(Color.BLACK);
+				}
+				if(textContra.getText().isEmpty()) {
+					textContra.setText("********");
+					textContra.setForeground(Color.GRAY);
+				}
+				if(textContraConfirm.getText().equals("********")) {
+					textContraConfirm.setText("");
+					textContraConfirm.setForeground(Color.BLACK);
+				}
+			}
+		});
+		textContraConfirm.setForeground(Color.GRAY);
+		textContraConfirm.setText("********");
+		textContraConfirm.setBorder(null);
+		textContraConfirm.setBounds(137, 190, 187, 27);
+		panel.add(textContraConfirm);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(137, 81, 203, 10);
+		panel.add(separator);
+		
+		JSeparator separator_1 = new JSeparator();
+		separator_1.setBounds(137, 153, 203, 10);
+		panel.add(separator_1);
+		
+		JSeparator separator_2 = new JSeparator();
+		separator_2.setBounds(137, 222, 203, 10);
+		panel.add(separator_2);
 		{
 			JButton okButton = new JButton("");
+			okButton.setBounds(186, 243, 98, 36);
+			panel.add(okButton);
 			okButton.setIcon(new ImageIcon(RegistroUsuario.class.getResource("/imagenes/guardar-el-archivo (1).png")));
-			okButton.setBounds(396, 318, 51, 36);
-			contentPanel.add(okButton);
 			okButton.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent arg0) {
 					
@@ -143,8 +249,49 @@ public class RegistroUsuario extends JDialog {
 			getRootPane().setDefaultButton(okButton);
 		}
 		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(0, 316, 495, 38);
-		contentPanel.add(panel_1);
+		lblEstudiante = new JLabel("");
+		lblEstudiante.setIcon(new ImageIcon(RegistroUsuario.class.getResource("/imagenes/leer (1).png")));
+		lblEstudiante.setBounds(22, 26, 98, 95);
+		panel.add(lblEstudiante);
+		
+		lblProfesor = new JLabel("");
+		lblProfesor.setIcon(new ImageIcon(RegistroUsuario.class.getResource("/imagenes/profesor (1).png")));
+		lblProfesor.setBounds(27, 26, 98, 82);
+		panel.add(lblProfesor);
+		lblEstudiante.setVisible(false);
+		lblProfesor.setVisible(false);
+		
+		panelExit = new JPanel();
+		panelExit.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				panelExit.setBackground(Color.RED);
+				xExit.setForeground(Color.BLACK);
+				
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				panelExit.setBackground(Color.BLUE);
+				xExit.setForeground(Color.WHITE);
+			}
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				dispose();
+			}
+		});
+		panelExit.setBackground(Color.BLUE);
+		panelExit.setBounds(482, 0, 31, 30);
+		contentPanel.add(panelExit);
+		
+		xExit = new JLabel("X");
+		xExit.setForeground(Color.WHITE);
+		xExit.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		panelExit.add(xExit);
+		
+		lblNewLabel_1 = new JLabel("Registro");
+		lblNewLabel_1.setForeground(Color.WHITE);
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 21));
+		lblNewLabel_1.setBounds(203, 13, 107, 29);
+		contentPanel.add(lblNewLabel_1);
 	}
 }
